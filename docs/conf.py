9d25a328f1c84b3f885f18a34f293340691d47da
paoloaizoon <paolo.perra@consulenti.csi.it>
2023-10-06 16:24:50 +0200

    231006commit.19

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,8 @@ pygments_style = None
 extensions = [ 'sphinx_rtd_theme' ]
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["/home/paolo/.local/lib/python3.10/site-packages/sphinx_rtd_theme/"]
+#html_theme_path = ["/home/paolo/.local/lib/python3.10/site-packages/sphinx_rtd_theme/"]
+html_theme_path = ["_themes", ]
 
 html_static_path = ['_static']
 
