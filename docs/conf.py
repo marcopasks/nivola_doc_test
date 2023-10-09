cdfaea3ecb2c87f2b9ee40edf2391709d301e892
paoloaizoon <paolo.perra@consulenti.csi.it>
2023-10-09 12:27:12 +0200

    231009commit.14

--- docs/conf.py
+++ docs/conf.py
@@ -105,7 +105,7 @@ html_theme_options = {
     'includehidden': False,
     'titles_only': False
 }
-html_favicon = 'favicon.ico'
+#html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
