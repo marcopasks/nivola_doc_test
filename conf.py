0c3be7951e988b6b33d4b1b0fca4dd66133317ae
paoloaizoon <paolo.perra@consulenti.csi.it>
2023-10-26 10:54:57 +0200

    231026commit.1

--- conf.py
+++ conf.py
@@ -53,3 +53,9 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+def setup(app):
+    #app.add_stylesheet("css/nivola.css")
+    app.add_css_file("css/nivola.css")
+
+#html_logo = '_static/img/logonivola.png' se attivo visualizza doppio logo
