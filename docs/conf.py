4408d56be3f756302568c6518095913fc919e785
Massimo Carbone <massimo.carbone@alten.it>
2023-12-22 13:10:28 +0100

    231222commit-MC.28

--- docs/conf.py
+++ docs/conf.py
@@ -32,8 +32,8 @@ release = '1.0.0'
 # ones.
 extensions = [
     'sphinx.ext.ifconfig',
-    # 'sphinx.ext.intersphinx',
-    'sphinxcontrib.jquery',
+     'sphinx.ext.intersphinx',
+    #'sphinxcontrib.jquery',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
