5abfbdc38383801b3c0b25cc34664efff482c1bf
Massimo Carbone <massimo.carbone@alten.it>
2023-12-22 16:07:44 +0100

    231222commit-MC.41

--- docs/conf.py
+++ docs/conf.py
@@ -31,10 +31,10 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+      'sphinx_rtd_theme' ,
     'sphinx.ext.ifconfig',
      'sphinx.ext.intersphinx',
     #  'sphinxcontrib.jquery',
-      'sphinx_rtd_theme' 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
