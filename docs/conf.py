335864e4552c7f66a6a8cc665ca41de068ca2109
Massimo Carbone <massimo.carbone@alten.it>
2024-12-12 10:34:20 +0100

    Revert "20241211 - MC: fix ricerca 16"
    This reverts commit c61262cd0b76996639d719428f3dcae7f3009765.

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
 
 # -- Project information -----------------------------------------------------
 
